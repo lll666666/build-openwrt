ab0f9eb1cec0a8ee585df3aa75b18091dfdd3348
lll <98231341+lll666666@users.noreply.github.com>
2024-01-08 21:23:32 +0800

    Update diy-part2.sh

--- config/openwrt-main/diy-part2.sh
+++ config/openwrt-main/diy-part2.sh
@@ -24,19 +24,20 @@ echo "DISTRIB_SOURCECODE='official'" >>package/base-files/files/etc/openwrt_rele
 #
 # Add luci-app-amlogic
 # svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+# echo 'src-git istore https://github.com/linkease/istore' >>feeds.conf.default
+echo 'src-git-full packages https://github.com/jjm2473/packages.git^b26cfeee1' >>feeds.conf.default
+echo 'src-git-full luci https://github.com/jjm2473/luci.git^5b482a70ba' >>feeds.conf.default
+echo 'src-git-full routing https://git.openwrt.org/feed/routing.git^777c115' >>feeds.conf.default
+echo 'src-git-full telephony https://git.openwrt.org/feed/telephony.git^8143d9d' >>feeds.conf.default
+echo 'src-git store https://github.com/linkease/istore.git^4c5098f' >>feeds.conf.default
+echo 'src-git third https://github.com/jjm2473/openwrt-third.git^4a0f27e' >>feeds.conf.default
+echo 'src-git third_party https://github.com/linkease/istore-packages.git^3874e89' >>feeds.conf.default
+echo 'src-git diskman https://github.com/jjm2473/luci-app-diskman.git^113509f' >>feeds.conf.default
+echo 'src-git oaf https://github.com/jjm2473/OpenAppFilter.git^d8663f9' >>feeds.conf.default
+echo 'src-git linkease_nas https://github.com/linkease/nas-packages.git^87bf6e6' >>feeds.conf.default
+echo 'src-git linkease_nas_luci https://github.com/linkease/nas-packages-luci.git^f9869e2' >>feeds.conf.default
+echo 'src-git jjm2473_apps https://github.com/jjm2473/openwrt-apps.git^fae9cde' >>feeds.conf.default
 svn co https://github.com/lll666666/666 package/luci-app-openclash
-src-git-full packages https://github.com/jjm2473/packages.git^b26cfeee1
-src-git-full luci https://github.com/jjm2473/luci.git^5b482a70ba
-src-git-full routing https://git.openwrt.org/feed/routing.git^777c115
-src-git-full telephony https://git.openwrt.org/feed/telephony.git^8143d9d
-src-git store https://github.com/linkease/istore.git^4c5098f
-src-git third https://github.com/jjm2473/openwrt-third.git^4a0f27e
-src-git third_party https://github.com/linkease/istore-packages.git^3874e89
-src-git diskman https://github.com/jjm2473/luci-app-diskman.git^113509f
-src-git oaf https://github.com/jjm2473/OpenAppFilter.git^d8663f9
-src-git linkease_nas https://github.com/linkease/nas-packages.git^87bf6e6
-src-git linkease_nas_luci https://github.com/linkease/nas-packages-luci.git^f9869e2
-src-git jjm2473_apps https://github.com/jjm2473/openwrt-apps.git^fae9cde
 
 
 # coolsnowwolf default software package replaced with Lienol related software package
